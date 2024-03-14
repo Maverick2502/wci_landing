05535eaffde28d24b094219b09a821a208119d3b
Nurullo <khuwaidullaev@gmail.com>
2024-03-14 23:44:16 +0500

    chore: download fonts (not working yet)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from "astro/config";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      VitePluginFonts({
+        custom: {
+          families: [
+            {
+              name: "Asgard Trial",
+              src: "./src/assets/fonts/*.woff",
+            },
+          ],
+          display: "swap",
+          preload: true,
+          prefetch: false,
+          injectTo: "head-prepend",
+        },
+      }),
+    ],
+  },
+});
