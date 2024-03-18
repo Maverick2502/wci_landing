5f1bcfba22db986ca3121e92ff26ee8ae4f3698d
Nurullo <khuwaidullaev@gmail.com>
2024-03-18 23:41:17 +0500

    style: add hero's widgets

--- src/shared/Card.tsx
+++ src/shared/Card.tsx
@@ -1,13 +1,9 @@
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 function Card() {
-  const { t } = useTranslation("translation");
+  // const { t } = useTranslation("translation");
 
-  return (
-    <div>
-      <pre>{t("menu")}</pre>
-    </div>
-  );
+  return <div>REACT</div>;
 }
 
 export default Card;
