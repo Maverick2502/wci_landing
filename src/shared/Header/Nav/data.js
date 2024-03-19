4bb1ac80659119d3c1d611108dbd50d70635479e
Nurullo <khuwaidullaev@gmail.com>
2024-03-19 16:56:27 +0500

    chore: add side menu

---
+++ src/shared/Header/Nav/data.js
@@ -0,0 +1,41 @@
+export const links = [
+  {
+    title: "Projects",
+    href: "/",
+  },
+  {
+    title: "Agency",
+    href: "/",
+  },
+  {
+    title: "Expertise",
+    href: "/",
+  },
+  {
+    title: "Careers",
+    href: "/",
+  },
+  {
+    title: "Contact",
+    href: "/",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "LinkedIn",
+    href: "/",
+  },
+  {
+    title: "Instagram",
+    href: "/",
+  },
+  {
+    title: "Facebook",
+    href: "/",
+  },
+  {
+    title: "Get call",
+    href: "/",
+  },
+];
