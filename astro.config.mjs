56bb3afc92b338ac26d8241876575f48a0847871
Nurullo <khuwaidullaev@gmail.com>
2024-03-19 11:43:32 +0500

    feat: add shadcn/ui, reinstall react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { VitePluginFonts } from "vite-plugin-fonts";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), , react()],
+  integrations: [astroI18next(), react(), tailwind()],
   prefetch: true,
   vite: {
     plugins: [
