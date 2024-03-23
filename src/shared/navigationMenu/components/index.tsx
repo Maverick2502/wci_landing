48a9318b165c355631b3a3fc21f986bd214cf0c0
Nurullo <khuwaidullaev@gmail.com>
2024-03-24 02:34:35 +0500

    refactor: create navigationMenu component, update navigationMenu component, cleanup

--- src/shared/navigationMenu/components/index.tsx
+++ src/shared/navigationMenu/components/index.tsx
@@ -1,47 +1,30 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import { menu } from "../utils/anim";
 import { Button } from "./Button";
 import { Menu } from "./Menu";
-import styles from "./styles.module.scss";
+import "./styles.scss";
 
-const menu = {
-  open: {
-    width: "460px",
-    height: "560px",
-    top: "-20px",
-    right: "-20px",
-    borderRadius: "15px",
-    transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
-  },
-  closed: {
-    width: "138px",
-    height: "40px",
-    top: "0px",
-    right: "0px",
-    borderRadius: "15px",
-    transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1] },
-  },
-};
-
-export default function NavigationMenu() {
+function NavigationMenu() {
   const [isActive, setIsActive] = useState(false);
 
+  const handleToggle = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className={styles["header"]}>
+    <div className="nav-menu-container">
       <motion.div
-        className={styles["header__menu"]}
-        variants={menu}
         animate={isActive ? "open" : "closed"}
+        className="nav-menu-container__content"
+        variants={menu}
         initial="closed"
       >
         <AnimatePresence>{isActive && <Menu />}</AnimatePresence>
       </motion.div>
-      <Button
-        isActive={isActive}
-        toggleMenu={() => {
-          setIsActive(!isActive);
-        }}
-      />
+      <Button isActive={isActive} toggleMenu={handleToggle} />
     </div>
   );
 }
+
+export default NavigationMenu;
--- src/shared/navigationMenu/utils/anim.ts
+++ src/shared/navigationMenu/utils/anim.ts
@@ -1,3 +1,22 @@
+export const menu = {
+  open: {
+    width: "460px",
+    height: "560px",
+    top: "-20px",
+    right: "-20px",
+    borderRadius: "15px",
+    transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
+  },
+  closed: {
+    width: "138px",
+    height: "60px",
+    top: "0px",
+    right: "0px",
+    borderRadius: "15px",
+    transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1] },
+  },
+};
+
 export const perspective = {
   initial: {
     opacity: 0,
