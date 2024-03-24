0e7f2b0a25ce359e8c4b21405c4967ac9ab6b6f1
Nurullo <khuwaidullaev@gmail.com>
2024-03-25 01:47:03 +0500

    chore: remove VIteFontPlugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,11 @@
-import { defineConfig } from "astro/config";
-import { VitePluginFonts } from "vite-plugin-fonts";
-import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next(), react()],
   vite: {
-    plugins: [
-      VitePluginFonts({
-        custom: {
-          families: [
-            {
-              name: "Asgard Trial",
-              src: "./src/assets/fonts/*.woff",
-            },
-          ],
-          display: "swap",
-          preload: true,
-          prefetch: false,
-          injectTo: "head-prepend",
-        },
-      }),
-    ],
+    plugins: [],
   },
 });
