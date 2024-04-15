39073fe8232a5bc9ec4b757f322695e1ab772a6c
Nurullo <khuwaidullaev@gmail.com>
2024-04-15 17:10:45 +0500

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), react()],
+  site: "https://wci-landing.vercel.app/",
+  integrations: [
+    astroI18next(),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "ru",
+        locales: {
+          ru: "ru",
+          en: "en",
+        },
+      },
+    }),
+  ],
 });
