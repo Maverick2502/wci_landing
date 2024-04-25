e6cd7eb822bed17a6de10e9c16786fe8261678ae
Nurullo <khuwaidullaev@gmail.com>
2024-04-25 15:56:28 +0500

    package: update astrojs to 4.7.0

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["feat", "chore", "fix", "doc", "style", "refactor", "test", "revert", "build", "ci", "perf", "seo"],
+      ["feat", "chore", "fix", "doc", "style", "refactor", "test", "revert", "build", "ci", "perf", "seo", "package"],
     ],
   },
 };
